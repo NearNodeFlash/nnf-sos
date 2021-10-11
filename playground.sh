2df91d3b9dd680bd03b1754bcee909031696e766
Dean Roehrich <droehrich@cray.com>
2021-10-11 08:44:50 -0500

    Merge pull request #38 in RABSW/nnf-sos from RABSW-573 to master
    Squashed commit of the following:
    
    commit d802a9c054aea30a9bc245b5d0819bdebb791330
    Author: Dean Roehrich <dean.roehrich@hpe.com>
    Date:   Fri Oct 8 11:54:33 2021 -0500
    
        RABSW-573 Let nnf-sos playground.sh supercede nnf-sos-playground
    
        Merge the nnf-sos-playground cmd.sh content into  the nnf-sos playground.sh,
        and retire nnf-sos-playground.

--- playground.sh
+++ playground.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 CMD=$1
-if [ $CMD == "kind-destroy" ]; then
-    kind delete cluster
-fi
 
-# Create a kind-cluster consisting of
+SUB_PKGS=(
+    ".dws-operator"
+    "."
+)
+
 if [ $CMD == "kind-create" ]; then
     CONFIG=kind-config.yaml
     if [ ! -f "$CONFIG" ]; then
@@ -22,20 +23,24 @@ nodes:
 EOF
     fi
 
-    kind create cluster --wait 60s --config kind-config.yaml
+    kind create cluster --wait 60s --image=kindest/node:v1.20.0 --config kind-config.yaml
 
     # Let our first kind-worker double as a generic worker for the SLC and as a
     # rabbit for the NLC.
     kubectl label node kind-worker cray.nnf.manager=true
     kubectl label node kind-worker cray.wlm.manager=true
 
-    for NODE in $(kubectl get nodes --no-headers | awk '{print $1;}' | grep --invert-match "control-plane"); do
+    for NODE in $(kubectl get nodes --no-headers | grep --invert-match "control-plane" | awk '{print $1}'); do
         kubectl label node $NODE cray.nnf.node=true
         kubectl label node $NODE cray.nnf.x-name=$NODE
     done
 
     #Required for webhooks
     kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.4.0/cert-manager.yaml
+fi
+
+if [ $CMD == "kind-destroy" ]; then
+    kind delete cluster
 fi
 
 if [ $CMD == "kind-reset" ]; then
@@ -47,10 +52,68 @@ if [ $CMD == "busybox" ]; then
     kubectl run -it --rm --restart=Never busybox --image=radial/busyboxplus:curl sh
 fi
 
-# Other useful commands are in the Makefile. Noteably
-# make docker-build kind-push
-# make deploy
-# make undeploy
+if [ $CMD == "docker-build" ] || [ $CMD == "kind-push" ] || [ $CMD == "deploy" ]; then
+    for PKG in ${SUB_PKGS[@]}; do
+        ( cd $PKG; make $CMD )
+    done
+fi
+
+if [ $CMD == "undeploy" ]; then
+    # Reverse the list.
+    for PKG in $(echo ${SUB_PKGS[@]} | tr ' ' '\n' | tac); do
+        ( cd $PKG; make $CMD )
+    done
+fi
+
+if [[ $CMD == pause || $CMD == resume ]]; then
+    COMP="$2"
+
+    do_node_manager=no
+    do_controller_manager=no
+    do_dws_operator=no
+
+    case "$COMP" in
+    node-manager)
+        do_node_manager=yes
+        ;;
+    controller-manager)
+        do_controller_manager=yes
+        ;;
+    dws-operator)
+        do_dws_operator=yes
+        ;;
+    all)
+        do_controller_manager=yes
+        do_node_manager=yes
+        do_dws_operator=yes
+        ;;
+    *)
+        echo "Specify: node-manager, controller-manager, dws-operator, or all"
+        exit 1
+        ;;
+    esac
+
+    if [[ $CMD == pause ]]; then
+        [[ $do_node_manager == yes ]] && kubectl patch daemonset -n nnf-system nnf-node-manager -p '{"spec":{"template":{"spec":{"nodeSelector":{"cray.nnf.node":"false"}}}}}'
+        [[ $do_controller_manager == yes ]] && kubectl scale deployment --replicas=0 -n nnf-system nnf-controller-manager
+        [[ $do_dws_operator == yes ]] && kubectl scale deployment --replicas=0 -n dws-operator-system dws-operator-controller-manager
+    fi
+    if [[ $CMD == resume ]]; then
+        [[ $do_node_manager == yes ]] && kubectl patch daemonset -n nnf-system nnf-node-manager -p '{"spec":{"template":{"spec":{"nodeSelector":{"cray.nnf.node":"true"}}}}}'
+        [[ $do_controller_manager == yes ]] && kubectl scale deployment --replicas=1 -n nnf-system nnf-controller-manager
+        [[ $do_dws_operator == yes ]] && kubectl scale deployment --replicas=1 -n dws-operator-system dws-operator-controller-manager
+    fi
+fi
+
+if [[ $CMD == list ]]; then
+    kinds_dws=$(kubectl api-resources | grep -i dws | awk '{print $1}' | sort)
+    kinds_nnf=$(kubectl api-resources | grep -i nnf | awk '{print $1}' | sort)
+    for x in $kinds_dws $kinds_nnf; do
+        echo "=== $x"
+        kubectl get -A $x
+        echo
+    done
+fi
 
 if [ $CMD == "slc-create" ]; then
     kubectl apply -f config/samples/nnf_v1alpha1_storage.yaml
