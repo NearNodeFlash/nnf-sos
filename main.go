322bb4a30229bcdc10e4fc767f7db2c0fa345649
Dean Roehrich <dean.roehrich@hpe.com>
2023-04-10 10:49:51 -0500

    main.go has too many calls to controllers.NnfPortManagerReconciler (#178)
    Keep the one for the SLC, remove the one in main().
    
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- main.go
+++ main.go
@@ -125,13 +125,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controllers.NnfPortManagerReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "NnfPortManager")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
