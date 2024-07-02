da4b5a5a6c1b4f7394ee891876dc9dd5e3b5f285
Dean Roehrich <dean.roehrich@hpe.com>
2024-07-02 13:05:26 -0500

    Add version labels to release manifest items (#321)
    Add component/version labels to each resource in the manifest. If run
    locally the labels will be:
    
      app.kubernetes.io/component: nnf-sos
      app.kubernetes.io/version: <git-version-gen>
    
    If run from nnf-deploy's "make manifests" the labels will be:
    
      app.kubernetes.io/component: nnf-sos
      app.kubernetes.io/version: <nnf-sos's git-version-gen>
      app.kubernetes.io/part-of: nnf
      app.kubernetes.io/nnf-version: <nnf-deploy's git-version-gen>
    
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- hack/make-kustomization.sh
+++ hack/make-kustomization.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2023 Hewlett Packard Enterprise Development LP
+# Copyright 2023-2024 Hewlett Packard Enterprise Development LP
 # Other additional copyright holders may be indicated within.
 #
 # The entirety of this work is licensed under the Apache License,
@@ -23,6 +23,7 @@ OVERLAY_DIR=$1
 OVERLAY=$2
 IMAGE_TAG_BASE=$3
 TAG=$4
+SOS_VERSION=$5
 
 if [[ ! -d $OVERLAY_DIR ]]
 then
@@ -32,6 +33,21 @@ fi
 cat <<EOF > "$OVERLAY_DIR"/kustomization.yaml
 resources:
 - ../$OVERLAY
+
+commonLabels:
+  app.kubernetes.io/version: "$SOS_VERSION"
+  app.kubernetes.io/component: nnf-sos
+EOF
+
+if [[ -n $NNF_VERSION ]]
+then
+    cat <<EOF >> "$OVERLAY_DIR"/kustomization.yaml
+  app.kubernetes.io/nnf-version: "$NNF_VERSION"
+  app.kubernetes.io/part-of: nnf
+EOF
+fi
+
+cat <<EOF >> "$OVERLAY_DIR"/kustomization.yaml
 
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
