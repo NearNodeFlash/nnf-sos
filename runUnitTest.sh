8a9a293e196ba4ffac5b219fee79a321fd0e99b9
Dean Roehrich <dean.roehrich@hpe.com>
2025-01-09 14:07:42 -0600

    Toss old unused tools (#434)
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- runUnitTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyright 2021 Hewlett Packard Enterprise Development LP
-
-# This is a magic file, whos presence is an idication to DST pipeline that
-# this repository should be running tests.
-
-make container-unit-test
-
