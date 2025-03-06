59048577395cc06fd233d3dbf0323d85bafb69ea
Blake Devcich <blake.devcich@hpe.com>
2025-03-06 15:48:59 -0600

    Add index to initcontainers

--- internal/controller/nnf_workflow_controller_container_helpers.go
+++ internal/controller/nnf_workflow_controller_container_helpers.go
@@ -298,7 +298,7 @@ func (c *nnfUserContainer) applyTolerations(spec *corev1.PodSpec) {
 	})
 }
 
-func (c *nnfUserContainer) addInitContainerPasswd(spec *corev1.PodSpec, image string) {
+func (c *nnfUserContainer) addInitContainerPasswd(spec *corev1.PodSpec, image string, idx int) {
 	// This script creates an entry in /etc/passwd to map the user to the given UID/GID using an
 	// InitContainer. This is necessary for mpirun because it uses ssh to communicate with the
 	// worker nodes. ssh itself requires that the UID is tied to a username in the container.
@@ -317,7 +317,7 @@ exit 0
 	script = strings.ReplaceAll(script, "$GID", fmt.Sprintf("%d", c.gid))
 
 	spec.InitContainers = append(spec.InitContainers, corev1.Container{
-		Name:  "mpi-init-passwd",
+		Name:  fmt.Sprintf("mpi-init-passwd-%d", idx),
 		Image: image,
 		Command: []string{
 			"/bin/sh",
@@ -414,7 +414,7 @@ func (c *nnfUserContainer) applyPermissions(spec *corev1.PodSpec, mpiJobSpec *mp
 		container := &spec.Containers[idx]
 
 		// Add an InitContainer to map the user to the provided uid/gid using /etc/passwd
-		c.addInitContainerPasswd(spec, container.Image)
+		c.addInitContainerPasswd(spec, container.Image, idx)
 
 		// Add a mount to copy the modified /etc/passwd to
 		container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
