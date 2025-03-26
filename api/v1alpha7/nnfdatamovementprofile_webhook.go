567521a017613f3731b70d52702385c69ceca802
Blake Devcich <blake.devcich@hpe.com>
2025-03-26 10:35:32 -0500

    CRDBUMPER-mv-webhooks
    Move the existing webhooks from v1alpha6 to v1alpha7.
    
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- api/v1alpha7/nnfdatamovementprofile_webhook.go
+++ api/v1alpha7/nnfdatamovementprofile_webhook.go
@@ -43,7 +43,7 @@ func (r *NnfDataMovementProfile) SetupWebhookWithManager(mgr ctrl.Manager) error
 
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate-nnf-cray-hpe-com-v1alpha6-nnfdatamovementprofile,mutating=false,failurePolicy=fail,sideEffects=None,groups=nnf.cray.hpe.com,resources=nnfdatamovementprofiles,verbs=create;update,versions=v1alpha6,name=vnnfdatamovementprofile.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-nnf-cray-hpe-com-v1alpha7-nnfdatamovementprofile,mutating=false,failurePolicy=fail,sideEffects=None,groups=nnf.cray.hpe.com,resources=nnfdatamovementprofiles,verbs=create;update,versions=v1alpha7,name=vnnfdatamovementprofile.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &NnfDataMovementProfile{}
 
