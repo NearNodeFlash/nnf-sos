cb1ce445b3debfdd03c9c9e80aee3844ee7f98b9
Matt Richerson <matthew.richerson@hpe.com>
2025-04-10 13:54:44 -0500

    cached compute to rabbit
    Signed-off-by: Matt Richerson <matthew.richerson@hpe.com>

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -23,14 +23,15 @@ import (
 	"context"
 
 	dwsv1alpha3 "github.com/DataWorkflowServices/dws/api/v1alpha3"
+	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// getRabbitFromCompute finds the name of the Rabbit that is physically connected to the compute node
-func GetRabbitFromCompute(ctx context.Context, c client.Client, compute string) (string, error) {
-	// To find the Rabbit associated with this compute, we have to search the SystemConfiguration resource
+var computeRabbitMap map[string]string
+
+func initializeComputeRabbitMap(ctx context.Context, c client.Client) error {
 	systemConfiguration := &dwsv1alpha3.SystemConfiguration{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "default",
@@ -39,17 +40,40 @@ func GetRabbitFromCompute(ctx context.Context, c client.Client, compute string) 
 	}
 
 	if err := c.Get(ctx, client.ObjectKeyFromObject(systemConfiguration), systemConfiguration); err != nil {
-		return "", dwsv1alpha3.NewResourceError("could not get SystemConfiguration %v", client.ObjectKeyFromObject(systemConfiguration)).WithError(err).WithMajor()
+		return dwsv1alpha3.NewResourceError("could not get SystemConfiguration %v", client.ObjectKeyFromObject(systemConfiguration)).WithError(err).WithMajor()
 	}
 
-	// Search through the SystemConfiguration to find the Compute node name
+	computeRabbitMap = make(map[string]string)
+
+	// Walk through the SystemConfiguration and build a map of compute to Rabbit names
 	for _, storageNode := range systemConfiguration.Spec.StorageNodes {
 		for _, computeAccess := range storageNode.ComputesAccess {
-			if computeAccess.Name == compute {
-				return storageNode.Name, nil
-			}
+			computeRabbitMap[computeAccess.Name] = storageNode.Name
 		}
 	}
 
-	return "", dwsv1alpha3.NewResourceError("could not find compute in SystemConfiguration: %s", compute).WithMajor()
+	return nil
+}
+
+// getRabbitFromCompute finds the name of the Rabbit that is physically connected to the compute node
+func GetRabbitFromCompute(ctx context.Context, c client.Client, compute string) (string, error) {
+	var log logr.Logger
+	log.Info("Rabbit from compute", "compute", compute)
+	if len(computeRabbitMap) == 0 {
+		log.Info("empty map")
+		err := initializeComputeRabbitMap(ctx, c)
+		if err != nil {
+			log.Info("error initializing")
+			return "", err
+		}
+	}
+
+	rabbit, found := computeRabbitMap[compute]
+	if !found {
+		log.Info("could not find Rabbit for compute", "compute", compute)
+		return "", dwsv1alpha3.NewResourceError("could not find compute in SystemConfiguration: %s", compute).WithMajor()
+	}
+
+	log.Info("found", "compute", compute, "rabbit", rabbit)
+	return rabbit, nil
 }
