8c51cc5dac7255020a8c79f9884c4093fa87fc78
Matt Richerson <matthew.richerson@hpe.com>
2025-07-15 09:01:47 -0500

    CRDBUMPER-mv-webhooks
    Move the existing webhooks from v1alpha7 to v1alpha8.
    
    Signed-off-by: Matt Richerson <matthew.richerson@hpe.com>

--- api/v1alpha8/nnfstorageprofile_webhook.go
+++ api/v1alpha8/nnfstorageprofile_webhook.go
@@ -42,7 +42,7 @@ func (r *NnfStorageProfile) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-//+kubebuilder:webhook:path=/validate-nnf-cray-hpe-com-v1alpha7-nnfstorageprofile,mutating=false,failurePolicy=fail,sideEffects=None,groups=nnf.cray.hpe.com,resources=nnfstorageprofiles,verbs=create;update,versions=v1alpha7,name=vnnfstorageprofile.kb.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-nnf-cray-hpe-com-v1alpha8-nnfstorageprofile,mutating=false,failurePolicy=fail,sideEffects=None,groups=nnf.cray.hpe.com,resources=nnfstorageprofiles,verbs=create;update,versions=v1alpha8,name=vnnfstorageprofile.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &NnfStorageProfile{}
 
