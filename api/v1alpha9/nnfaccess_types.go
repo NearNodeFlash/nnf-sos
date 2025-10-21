f61fd8286e8572a9e390863733f62a86789c9af0
Matt Richerson <matthew.richerson@hpe.com>
2025-10-21 09:12:25 -0500

    CRDBUMPER-create-apis
    Create v1alpha9 APIs.
    
    This used "kubebuilder create api --resource --controller=false"
    for each API.
    
    Signed-off-by: Matt Richerson <matthew.richerson@hpe.com>

---
+++ api/v1alpha9/nnfaccess_types.go
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2025 Hewlett Packard Enterprise Development LP
+ * Other additional copyright holders may be indicated within.
+ *
+ * The entirety of this work is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1alpha9
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NnfAccessSpec defines the desired state of NnfAccess
+type NnfAccessSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of NnfAccess. Edit nnfaccess_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// NnfAccessStatus defines the observed state of NnfAccess
+type NnfAccessStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// NnfAccess is the Schema for the nnfaccesses API
+type NnfAccess struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NnfAccessSpec   `json:"spec,omitempty"`
+	Status NnfAccessStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// NnfAccessList contains a list of NnfAccess
+type NnfAccessList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NnfAccess `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NnfAccess{}, &NnfAccessList{})
+}
