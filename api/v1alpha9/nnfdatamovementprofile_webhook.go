c175636e6256007019ccf48c79e518bee0ba0772
Matt Richerson <matthew.richerson@hpe.com>
2025-10-21 09:12:27 -0500

    CRDBUMPER-mv-webhooks
    Move the existing webhooks from v1alpha8 to v1alpha9.
    
    Signed-off-by: Matt Richerson <matthew.richerson@hpe.com>

--- api/v1alpha9/nnfdatamovementprofile_webhook.go
+++ api/v1alpha9/nnfdatamovementprofile_webhook.go
@@ -50,7 +50,7 @@ var (
 
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate-nnf-cray-hpe-com-v1alpha8-nnfdatamovementprofile,mutating=false,failurePolicy=fail,sideEffects=None,groups=nnf.cray.hpe.com,resources=nnfdatamovementprofiles,verbs=create;update,versions=v1alpha8,name=vnnfdatamovementprofile.kb.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-nnf-cray-hpe-com-v1alpha9-nnfdatamovementprofile,mutating=false,failurePolicy=fail,sideEffects=None,groups=nnf.cray.hpe.com,resources=nnfdatamovementprofiles,verbs=create;update,versions=v1alpha9,name=vnnfdatamovementprofile.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &NnfDataMovementProfile{}
 
